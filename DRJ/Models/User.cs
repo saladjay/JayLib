b39d7a87fcc9c9ec688680a8eb145b842882187e
雍杰 邓 <dyj394041771@sina.com>
2020-06-01 23:53:21 +0800

    first update

---
+++ DRJ/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JayLib.WPF.BasicClass;
+
+namespace DRJ.Models
+{
+    public class User : NotificationObject
+    {
+        private string account;
+
+        public string Account
+        {
+            get { return account; }
+            private set { account = value; }
+        }
+
+        private int authorizationLevel;
+
+        public int AuthorizationLevel
+        {
+            get { return authorizationLevel; }
+            set { authorizationLevel = value; }
+        }
+
+    }
+}
