f2c8b20a1a564fc4283ebc2feffe0be570a9f4c1
雍杰 邓 <dyj394041771@sina.com>
2020-06-16 18:07:01 +0800

    update

---
+++ DRJ/MainWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using JayLib.WPF.BasicClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace DRJ
+{
+    public class MainWindowViewModel : NotificationObject
+    {
+
+        #region floating Save button demo
+
+
+        private bool _isSaving;
+        public bool IsSaving
+        {
+            get { return _isSaving; }
+            private set { this.PropertyUpdate(ref _isSaving, value, () => IsSaving); }
+        }
+
+        private bool _isSaveComplete;
+        public bool IsSaveComplete
+        {
+            get { return _isSaveComplete; }
+            private set { this.PropertyUpdate(ref _isSaveComplete, value, () => IsSaveComplete); }
+        }
+
+        private double _saveProgress;
+        public double SaveProgress
+        {
+            get { return _saveProgress; }
+            private set { this.PropertyUpdate(ref _saveProgress, value, () => SaveProgress); }
+        }
+
+        #endregion
+
+        public ICommand SaveCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            //just some demo code for the SAVE button
+            SaveCommand = new RelayCommand(SaveExecute);
+        }
+
+        private void SaveExecute()
+        {
+            if (IsSaveComplete == true)
+            {
+                IsSaveComplete = false;
+                return;
+            }
+
+            if (SaveProgress != 0) return;
+
+            var started = DateTime.Now;
+            IsSaving = true;
+
+            new DispatcherTimer(
+                TimeSpan.FromMilliseconds(50),
+                DispatcherPriority.Normal,
+                new EventHandler((o, e) =>
+                {
+                    var totalDuration = started.AddSeconds(3).Ticks - started.Ticks;
+                    var currentProgress = DateTime.Now.Ticks - started.Ticks;
+                    var currentProgressPercent = 100.0 / totalDuration * currentProgress;
+
+                    SaveProgress = currentProgressPercent;
+
+                    if (SaveProgress >= 100)
+                    {
+                        IsSaveComplete = true;
+                        IsSaving = false;
+                        SaveProgress = 0;
+                        ((DispatcherTimer)o).Stop();
+                    }
+
+                }), Dispatcher.CurrentDispatcher);
+        }
+    }
+}
--- JayLib/WPF.BasicClass/NotificationObject.cs
+++ JayLib/WPF.BasicClass/NotificationObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,46 @@ namespace JayLib.WPF.BasicClass
         {
             var propertyName = (propertyExpression.Body as MemberExpression).Member.Name;
             this.OnPropertyChanged(propertyName);
+        }
+
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged()
+        {
+            return args => PropertyChanged?.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// 自带检查泛型值检查，如果该属性没有改变则不发起事件。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="propertyName"></param>
+        protected void PropertyUpdate<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            this.MutateVerbose(ref field, newValue, RaisePropertyChanged(), propertyName);
+        }
+
+        /// <summary>
+        /// 自带检查泛型值检查，如果该属性没有改变则不发起事件。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="propertyName"></param>
+        protected void PropertyUpdate<T>(ref T field, T newValue, Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = (propertyExpression.Body as MemberExpression).Member.Name;
+            this.MutateVerbose(ref field, newValue, RaisePropertyChanged(), propertyName);
+        }
+    }
+
+    public static class NotifyPropertyChangedExtension
+    {
+        public static void MutateVerbose<TField>(this INotifyPropertyChanged instance, ref TField field, TField newValue, Action<PropertyChangedEventArgs> raise, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TField>.Default.Equals(field, newValue)) return;
+            field = newValue;
+            raise?.Invoke(new PropertyChangedEventArgs(propertyName));
         }
     }
 }
